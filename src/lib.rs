b3048164809971450bd8924ea01bad0291b48ad3
Chuck Bassett <iamchuckb@gmail.com>
2017-06-23 02:47:02 -0400

    Add models module with Tetriminos struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
 #[macro_use]
 mod macros;
+mod models;
+
+use models::Tetriminos;
 
 
-pub struct Game {}
+pub struct Game {
+    tetriminos: Tetriminos,
+}
 
 
 impl Game {
     pub fn run() {
-        let tetrimino_states = vec![
-            states!("O"),
-            states!("I"),
-            states!("T"),
-            states!("S"),
-            states!("Z"),
-            states!("J"),
-            states!("L"),
-        ];
-        for tetrimino in &tetrimino_states {
-            for state in tetrimino {
+        let game = Game {
+            tetriminos: Tetriminos::init(),
+        };
+        for (tet_type, states) in game.tetriminos.states() {
+            for state in states {
+                println!("{:?}", tet_type);
                 println!("{}", state);
             }
             println!("{}", "=".repeat(10));
---
+++ src/models.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+#[macro_use]
+use macros;
+
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub enum TetriminoType {
+    O,
+    I,
+    T,
+    S,
+    Z,
+    J,
+    L,
+}
+
+struct States {
+    states: HashMap<TetriminoType, Vec<String>>,
+}
+
+impl States {
+    pub fn init() -> States {
+        let tet_states: HashMap<TetriminoType, Vec<String>> = [
+            (TetriminoType::O, states!("O")),
+            (TetriminoType::I, states!("I")),
+            (TetriminoType::T, states!("T")),
+            (TetriminoType::S, states!("S")),
+            (TetriminoType::Z, states!("Z")),
+            (TetriminoType::J, states!("J")),
+            (TetriminoType::L, states!("L")),
+        ].iter().cloned().collect();
+        States {
+            states: tet_states,
+        }
+    }
+}
+
+pub struct Tetriminos {
+    states: States,
+}
+
+impl Tetriminos {
+    pub fn init() -> Tetriminos {
+        Tetriminos {
+            states: States::init(),
+        }
+    }
+    pub fn states(&self) -> &HashMap<TetriminoType, Vec<String>> {
+        &self.states.states
+    }
+}
